5837314f80464cfd4b22fae1165aac533d4e734c
Nkemdirim Enwerem <nkembabe.ne@gmail.com>
2022-10-16 09:00:33 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,21 @@
-include <stdio.h>
+#include <stdio.h>
 /**
-* main - print a c program using printf
-*
-* Return: 0 if no errors, return non zero if errors
+ * main - print out sizes of data types in c
+ *
+ * Return: 0 if no errors, return non zero if errors
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)size of(b));
+	printf("Size of an long int: %lu byte(s)\n", (unsigned long)size of(c));
+	printf("Size of an long long int: %lu byte(s)\n", (unsigned long)size of(d));
+	printf("Size of an float int: %lu byte(s)\n", (unsigned long)size of(f));
+	return (0);
 }
-		
