69d9314ee4996cfc61ac2b9ddbaf7aee8acbb83c
Nkemdirim Enwerem <nkembabe.ne@gmail.com>
2022-10-26 16:37:22 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
-/*
- * _puts - prints a string
- * @str: string to be printed
-*/
+/**
+ * _puts - Prints a string.
+ * @str: string.
+ */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
+	while (*str)
 		_putchar(*str++);
-	}
+
 	_putchar('\n');
 }
