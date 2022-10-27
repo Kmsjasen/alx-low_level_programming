015fd9c47634a961a316febc0997de1fa01b63ca
Nkemdirim Enwerem <nkembabe.ne@gmail.com>
2022-10-27 16:35:45 -0700

    task 14

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,43 +1,43 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2
- * followed by a new line
- * Return: ALways 0 (Success)
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: void.
  */
-int main(void)
+void print_buffer(char *b, int size)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	int i, j, k;
 
-	j = 1;
-	k = 2;
-
-	printf("%lu", j);
-
-	for (i = 1; i < 91; i++)
+	if (size <= 0)
+		printf("\n");
+	else
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		for (i = 0; i < size; i += 10)
+		{
+			printf("%.8x:", i);
+			for (j = i; j < i + 10; j++)
+			{
+				if (j % 2 == 0)
+					printf(" ");
+				if (j < size)
+					printf("%.2x", *(b + j));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (k = i; k < i + 10; k++)
+			{
+				if (k >= size)
+					break;
+				if (*(b + k) < 32 || *(b + k) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + k));
+			}
+			printf("\n");
+		}
 	}
-
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	k1 = k / 1000000000;
-	k2 = k % 1000000000;
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
-	}
-
-	printf("\n");
-
-	return (0);
 }
